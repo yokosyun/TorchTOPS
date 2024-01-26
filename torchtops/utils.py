60ccbd7b2b2c6bbf249848ffdce05c418d5ec9e5
yokosyun <yoko.syun.1408@gmail.com>
2024-01-26 11:26:36 +0900

    plot results

--- torchtops/utils.py
+++ torchtops/utils.py
@@ -1,7 +1,12 @@
 from typing import Dict, Any, List
 from functools import reduce
 from itertools import compress
-from torch import nn
+from torch import nn, Tensor
+import torch
+import statistics
+import matplotlib.pyplot as plt
+import numpy as np
+from pathlib import Path
 
 
 def get_module_by_layer_name(module: nn.Module, access_string: str) -> nn.Module:
@@ -21,3 +26,62 @@ def filter_modules(
             out_dict[key] = list(compress(val, masks))
 
     return out_dict
+
+
+def get_latency(
+    model: nn.Module,
+    input: Tensor,
+    iterations: int = 50,
+) -> float:
+    latencies = []
+    for _ in range(iterations):
+        start = torch.cuda.Event(enable_timing=True)
+        end = torch.cuda.Event(enable_timing=True)
+        start.record()
+        model(input)
+        end.record()
+        torch.cuda.synchronize()
+        latencies.append(start.elapsed_time(end))
+    return statistics.median(latencies)  # / 1.0e3
+
+
+def plot_results(res: Dict[str, Any], worst_k: int, save_path: str) -> None:
+    x = res["layer_names"][:worst_k]
+
+    fig, (ax1, ax2, ax3, ax4, ax5, ax6, ax7) = plt.subplots(
+        7, sharex="col", figsize=(10, 16), constrained_layout=True
+    )
+    ax1.bar(x, np.array(res["latencies"][:worst_k]) / res["total_latency"] * 100)
+    ax1.set_ylabel("latency[%]")
+
+    ax2.bar(x, np.array(res["tops_list"][:worst_k]) / res["total_tops"] * 100)
+    ax2.set_ylabel("TOPS[%]")
+
+    ax3.bar(
+        x,
+        np.array(res["arithmetric_intensity_list"][:worst_k])
+        / res["total_arithmetric_intensity"]
+        * 100,
+    )
+    ax3.set_ylabel("arith_intensity[%]")
+
+    ax4.bar(x, np.array(res["flops_list"][:worst_k]) / res["total_flops"] * 100)
+    ax4.set_ylabel("FLOPs[%]")
+
+    ax5.bar(x, np.array(res["params_list"][:worst_k]) / res["total_params"] * 100)
+    ax5.set_ylabel("params[%]")
+
+    ax6.bar(
+        x, np.array(res["read_counts_list"][:worst_k]) / res["total_read_counts"] * 100
+    )
+    ax6.set_ylabel("read_counts[%]")
+
+    ax7.bar(
+        x,
+        np.array(res["write_counts_list"][:worst_k]) / res["total_write_counts"] * 100,
+    )
+    ax7.set_ylabel("write_counts[%]")
+
+    plt.suptitle(Path(save_path).stem)
+    fig.autofmt_xdate()
+    fig.savefig(save_path)
